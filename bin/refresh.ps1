5794a4977608718777288508b8e931ade09f4912
dntsdnts <17964609+dntsdnts@users.noreply.github.com>
2024-07-03 04:37:11 +0900

    modified bin/refresh, libexec/scoop-update.ps1

--- bin/refresh.ps1
+++ bin/refresh.ps1
@@ -13,8 +13,41 @@ $output = robocopy $src $dest /mir /njh /njs /nfl /ndl /xd .git tmp /xf .DS_Stor
 $output | Where-Object { $_ -ne "" }
 
 Write-Output 'creating shim...'
-shim "$dest\bin\scoop.ps1" $FALSE
+shim "$dest\bin\scoop.ps1" $false
 
-#Set-AuthenticodeSignature "$dest\bin\scoop.ps1" (ls Cert:\CurrentUser\My\ -CodeSigningCert)
+Set-AuthenticodeSignature "$dest\bin\scoop.ps1" (ls Cert:\CurrentUser\My\ -CodeSigningCert)
 
 success 'scoop was refreshed!'
+
+# SIG # Begin signature block
+# MIIFcQYJKoZIhvcNAQcCoIIFYjCCBV4CAQExDzANBglghkgBZQMEAgEFADB5Bgor
+# BgEEAYI3AgEEoGswaTA0BgorBgEEAYI3AgEeMCYCAwEAAAQQH8w7YFlLCE63JNLG
+# KX7zUQIBAAIBAAIBAAIBAAIBADAxMA0GCWCGSAFlAwQCAQUABCDQOU6jt7Rcu0AX
+# lV5s+eJNaCg/grzTa196+L6hAVktZKCCAvIwggLuMIIB1qADAgECAhBRXjN43tOe
+# vkj4l+euvSLrMA0GCSqGSIb3DQEBDQUAMA8xDTALBgNVBAMMBHFycXIwHhcNMjQw
+# NjI5MDczMTE4WhcNMjUwNjI5MDc1MTE4WjAPMQ0wCwYDVQQDDARxcnFyMIIBIjAN
+# BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzGyCuR6iKpn8DX3kWN4b7mG9FwOf
+# P+3w/qAPET+0ejsqwRfd3PbQBtCln8LP40sTe0Oy5tOFez63/tXshModzgfA+5cA
+# iGG1I1YMVRHjpVPd24tZLr+6kkOR6az+VFS3zRCWhH/kN5oMxxkEt7vacZC1QRrh
+# PQWcCVXYorPmZwPNHws5k7ZxtPHWT367HZrzrzHXW0VB+XX52a7EgRWFVzAaCziH
+# DHUTAvnDwbnLGt1kfX43AxvcOPXpzFPtpEXh+DRgwKGjJaHKzuWYzK8lHs6TXbZF
+# QbJI4SN4xgq4+i2ceZECPl4ROzG9HaO7s4Q4TmeXAcyziMxb55QHQDauwQIDAQAB
+# o0YwRDAOBgNVHQ8BAf8EBAMCB4AwEwYDVR0lBAwwCgYIKwYBBQUHAwMwHQYDVR0O
+# BBYEFFxJWt2yBxX0gUBoRDAcm4HuLs9LMA0GCSqGSIb3DQEBDQUAA4IBAQBIqYh9
+# /0VLnlt0csz4RWJf6tpmdUrv39mlXfJXBQBgSjKrUNph1lyvEnXorTqCTyT5cjQ5
+# 5GXaN4jQYpE2FISWUte/b+JY0WPl5xS3Ewl5c6HVIwDZ/54hXKezQu18NVVRvbAL
+# 5blL+fn+NFMakRiP8Z/advmSN7qsF8H/HWSTRnkAAzfDe7folyzfgmej4Stk7XRX
+# QabaUPeiYTiJGhY0FFknsXLIwk3F0azE5LRxUD7qhoK2nFP9yPjVXqfkmxOt2WPo
+# 7FGDPJYS0iPB/oQO4/+3x0YHXgmE8BoicNRA9jQJ1s/gDQOX0qOWgbecdwNef1u/
+# Tnv+D9lQdt4kF86zMYIB1TCCAdECAQEwIzAPMQ0wCwYDVQQDDARxcnFyAhBRXjN4
+# 3tOevkj4l+euvSLrMA0GCWCGSAFlAwQCAQUAoIGEMBgGCisGAQQBgjcCAQwxCjAI
+# oAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYBBAGCNwIB
+# CzEOMAwGCisGAQQBgjcCARUwLwYJKoZIhvcNAQkEMSIEIGjDk0F1ZTsXOktcdQKi
+# leYa8amOkkewyKCyRS+nLgNjMA0GCSqGSIb3DQEBAQUABIIBADKxsk6kiC+N34Q1
+# 6RlywmKKQpdLAPvuscvFqSEuolfJlhNUGJMZIagsoVy3GKTKRSpOzR4B+4WWmp7X
+# N+b33/o4O4LcuT0Y9QTpUm4gnBp+9As8njydK0m0Vxvf+Jluyj4VM2poWBSl5jtX
+# R+T9+5dp0cjPy4BOa4Xi+69AdqHOqhgEZPyFZvmzfNQD2D0x269mUad6AiClVJMZ
+# NBSLINg0wI2iTJlBOKgp7U7+z+dRrKY33awIDi04jI+fqiDGhDPGVL7Dv/eenHhm
+# KxEEzXccs2CAHzdimUWQxFDnbiRAF3p7S+Pj3NqGp5fPW+rTC8FKLOQi+pRHIZM/
+# CUCeaB4=
+# SIG # End signature block
